3c4a9ff8bc72051ee09172d1a841a8f31a5cc769
ademide <ademideadeboye@gmail.com>
2023-02-17 08:42:27 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main - Prints and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
  * followed by a new line, to the standard error.
- *
  * Return: Always 1 (Success)
  */
 int main(void)
