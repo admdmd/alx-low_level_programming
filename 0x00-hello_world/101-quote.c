ce857954fa7ca70abde0c98913caee4659880835
ademide <ademideadeboye@gmail.com>
2023-02-17 08:46:59 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
- * followed by a new line, to the standard error.
+ * main - Entry point
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n", 59);
 return (1);
 }
 
