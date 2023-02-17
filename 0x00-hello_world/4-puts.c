61a860be2ad1956b263956165970c88404ec9267
ademide <ademideadeboye@gmail.com>
2023-02-16 17:40:15 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include < stdio.n>
+/**
+ * main - We are working with puts and not print
+ * return: 0 (signifying success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
