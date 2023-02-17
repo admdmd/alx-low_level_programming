b7b61aca9d1edc1bfb590b99fdfef511c7544ba4
ademide <ademideadeboye@gmail.com>
2023-02-17 06:11:56 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,12 @@
  */
 int main(void)
 {
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
 	printf("size of a char: %zu 1 byte(s)\n", sizeof(char));
 	printf("size of an int: %zu 4 byte(s)\n", sizeof(int));
 	printf("size of a long int: %zu 4 byte(s)\n", sizeof(long int));
