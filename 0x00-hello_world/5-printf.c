dd94528b440327126bc097600d8154064d8643bc
ademide <ademideadeboye@gmail.com>
2023-02-16 20:13:39 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - programe that prints using printf function;
- * Return: 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
