b12fac0a36d1d6fe4139f30f6de0a9d3a8fac972
ademide <ademideadeboye@gmail.com>
2023-02-23 17:29:06 -0800

    101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,7 +9,7 @@
 
 void print_number(int n)
 {
-unsigned int k = n
+unsigned int k = n;
 if (n < 0)
 {
 n *= -1;
@@ -19,5 +19,5 @@ _putchar('-');
 k /= 10;
 if (k != 0)
 print_number(k);
-_putchar (unsigned int) n % 10 + '0');
+_putchar((unsigned int) n % 10 + '0');
 }
