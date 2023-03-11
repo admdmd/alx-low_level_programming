4b0c2c1a0fde3d57708b803dd345bf33661f0b95
ademide <ademideadeboye@gmail.com>
2023-03-10 16:50:04 -0800

    libmy.a

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs(int)- compute the absolute value of an integer
+ * @r : the character
+ * Return: r if r is abs, else return -r
+ */
+int _abs(int r)
+{
+	if (r > 0)
+	{	return (r);
+	}
+	else
+		return (-r);
+}
