7334a037d14d4bd9642b634998c29103e04cf7f4
ademide <ademideadeboye@gmail.com>
2023-03-10 18:01:46 -0800

    0-whatsmyname.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - function that prints the n times table, starting with 0
+ *
+ * Return: 0
+ */
+void print_times_table(int n);
+{
+	if n >= 0 && n <= 15
+		printf("n * n")
+	else
+		Return(0)
+}
+
+
