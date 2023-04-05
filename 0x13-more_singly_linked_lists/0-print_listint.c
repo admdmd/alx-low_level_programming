2088e7e4a61827756a60055cd9ad4e490527ae0b
ademide <ademideadeboye@gmail.com>
2023-04-04 19:46:38 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - print number of nodes
+ * @h: pointer to the list
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t nodelen = 0;
+
+if (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+nodelen++;
+}
+return (nodelen);
+}
