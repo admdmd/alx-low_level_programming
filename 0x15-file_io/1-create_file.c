6e99a12fc2872e214de8ee9ddcd13455207b7c8d
ademide <ademideadeboye@gmail.com>
2023-05-09 01:58:05 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int y;
 	int z;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -21,10 +21,11 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (!text_content)
 		text_content = "";
 
 	for (y = 0; text_content[y]; y++)
+		;
 
 	z = write(fd, text_content, y);
 
