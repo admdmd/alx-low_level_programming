15bf49916370528b48f7b98d177645fa3489a56f
ademide <ademideadeboye@gmail.com>
2023-05-08 17:00:29 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - objective
+ * @filename: Name of target file
+ * @text_content: content to write to the file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int y;
+	int z;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (y = 0; text_content[y]; y++)
+
+	z = write(fd, text_content, y);
+
+	if (z == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
