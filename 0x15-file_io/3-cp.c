71a00a49326c85136b654a297cc9063e8247573f
ademide <ademideadeboye@gmail.com>
2023-05-11 15:51:29 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,83 +1,72 @@
 #include "main.h"
-#include <stdio.h>
 
-void copy_file(int file_from, int file_to);
+/**
+ * expandable - checks if files can be opened.
+ * @file_from: sending file
+ * @file_to: recieving file
+ * @argv: arguments vector
+ * Return: 0 on success
+ */
+int expandable(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	return (0);
+}
 
 /**
  * main - program that copies the content of a file to another file
- * @argc: counts arguments
- * @argv: counts arguemnt vector.
- * Return: 0 on success
+ * @ac: counts number of arguments
+ * @av: arguments vector.
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int ac, char *av[])
 {
-int file_from, file_to;
+	int file_from, file_to, man_file;
+	ssize_t y, z;
+	char buffer[1024];
 
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
+	if (ac < 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = open(av[1], O_RDONLY);
+	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	expandable(file_from, file_to, av);
+
+	y = 1024;
+	while (y == 1024)
+	{
+		y = read(file_from, buffer, 1024);
+		if (y == -1)
+			expandable(-1, 0, av);
+		z = write(file_to, buffer, y);
+		if (z == -1)
+			expandable(0, -1, av);
+	}
+
+	man_file = close(file_from);
+	if (man_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	man_file = close(file_to);
+	if (man_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
 }
-
-file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file_from\n");
-exit(98);
-}
-
-file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-if (file_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to file_to\n");
-exit(99);
-}
-
-copy_file(file_from, file_to);
-
-close(file_from);
-if (file_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
-}
-
-close(file_to);
-if (file_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
-}
-
-return (0);
-}
-
-/**
- * copy_file - copies the content of a file to another file
- * @file_from: source file descriptor
- * @file_to: destination file descriptor
- */
-void copy_file(int file_from, int file_to)
-{
-ssize_t nchar, nwr;
-char buffer[1024];
-
-nchar = 1024;
-
-while (nchar == 1024)
-{
-nchar = read(file_from, buffer, 1024);
-if (nchar == -1)
-{
-perror("Error: Can't read from file");
-exit(98);
-}
-nwr = write(file_to, buffer, nchar);
-if (nwr == -1)
-{
-perror("Error: Can't read from file");
-exit(99);
-}
-}
-}
-
